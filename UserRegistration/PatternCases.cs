f1d589e9b9ab0d8f0f36d9bedae404cfb0dc35c9
GollaVijayBabu <gollavijaybabu221@gmail.com>
2021-11-20 00:09:17 -0800

    Added UserRegistration

---
+++ UserRegistration/PatternCases.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+namespace UserRegistration
+{
+    public class PatternCheck
+    {
+        public const string patternForFName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public const string patternForLName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public const string patternForEmail = "^[a-z]{3,}([$#@!%?&*._-]?[a-z]+)?[@]{1}[a-z]{2,}[.]{1}[a-z]{2,}([.]?[a-z]+)?$";
+        public const string patternForPhoneNo = "^[1-9]{2}\\s[6-9]{1}[0-9]{9}$";
+        public const string patternForPassword = "^[A-Za-z0-9!@#$%^&*_-]{8,}$";
+        public const string patternForPwd = "[A-Z]{1,}[A-Za-z0-9!@#$%^&*_-]{7,}$";
+        public const string patternForPasWd = "[A-Z]{1,}[0-9]{1,}[A-Za-z0-9!@#$%^&*_-]{6,}$";
+        public const string patternForPaswd = "[A-Z]{1,}[0-9]{1,}[!@#$%^&*_-]{1,}[A-Za-z0-9!@#$%^&*_-]{5,}$";
+        public const string patternForAllEmail = "^[a-z]{3,}([!@#$%^+&*._-]?[0-9]+)?[@]{1}[0-9a-z]{1,}[.]{1}[a-z]{2,}([.]?[a-z]+)?$";
+
+        public bool FirstNameValidation(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, patternForFName);
+        }
+        public bool LastNameValidation(string LastName)
+        {
+            return Regex.IsMatch(LastName, patternForLName);
+        }
+        public bool EmailValidation(string Email)
+        {
+            return Regex.IsMatch(Email, patternForEmail);
+        }
+        public bool PhoneNoValidation(string PhoneNo)
+        {
+            return Regex.IsMatch(PhoneNo, patternForPhoneNo);
+        }
+        public bool UC_5_PasswordValidation(string Password)
+        {
+            return Regex.IsMatch(Password, patternForPassword);
+        }
+        public bool UC_6_PasswordValidation(string Password)
+        {
+            return Regex.IsMatch(Password, patternForPwd);
+        }
+        public bool UC_7_PasswordValidation(string Password)
+        {
+            return Regex.IsMatch(Password, patternForPasWd);
+        }
+        public bool UC_8_PasswordValidation(string Password)
+        {
+            return Regex.IsMatch(Password, patternForPaswd);
+        }
+        public bool allEmailValidation(string Email)
+        {
+            return Regex.IsMatch(Email, patternForAllEmail);
+        }
+    }
+}
--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,12 +1,107 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace UserRegistration
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Day 19 Program\n");
+            Console.WriteLine("Please selct following options\n");
+            Console.WriteLine(" 1.UC_1\n 2.UC_2\n 3.UC_3\n 4.UC_4\n 5.UC_5\n 6.UC_6\n 7.UC_7\n 8.UC_8\n 9.UC_9\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    PatternCheck FName = new PatternCheck();
+                    Console.WriteLine("Please enter fisrt name");
+                    string FirstName = Console.ReadLine();
+                    FName.FirstNameValidation(FirstName);
+                    Console.WriteLine(FName.FirstNameValidation(FirstName));
+                    break;
+                case 2:
+                    PatternCheck LName = new PatternCheck();
+                    Console.WriteLine("Please enter last name");
+                    string LastName = Console.ReadLine();
+                    LName.LastNameValidation(LastName);
+                    Console.WriteLine(LName.LastNameValidation(LastName));
+                    break;
+                case 3:
+                    PatternCheck Email = new PatternCheck();
+                    Console.WriteLine("Please enter your email");
+                    string email = Console.ReadLine();
+                    Email.EmailValidation(email);
+                    Console.WriteLine(Email.EmailValidation(email));
+                    break;
+                case 4:
+                    PatternCheck Phone = new PatternCheck();
+                    Console.WriteLine("Please enter your phone number with country code");
+                    string phoneNo = Console.ReadLine();
+                    Phone.PhoneNoValidation(phoneNo);
+                    Console.WriteLine(Phone.PhoneNoValidation(phoneNo));
+                    break;
+                case 5:
+                    PatternCheck PassWord = new PatternCheck();
+                    Console.WriteLine("Please enter your password");
+                    string passWord = Console.ReadLine();
+                    PassWord.UC_5_PasswordValidation(passWord);
+                    Console.WriteLine(PassWord.UC_5_PasswordValidation(passWord));
+                    break;
+                case 6:
+                    PatternCheck Pwd = new PatternCheck();
+                    Console.WriteLine("Please enter your password");
+                    string Pswd = Console.ReadLine();
+                    Pwd.UC_6_PasswordValidation(Pswd);
+                    Console.WriteLine(Pwd.UC_6_PasswordValidation(Pswd));
+                    break;
+                case 7:
+                    PatternCheck PasWd = new PatternCheck();
+                    Console.WriteLine("Please enter your password");
+                    string PaWd = Console.ReadLine();
+                    PasWd.UC_7_PasswordValidation(PaWd);
+                    Console.WriteLine(PasWd.UC_7_PasswordValidation(PaWd));
+                    break;
+                case 8:
+                    PatternCheck Paswd = new PatternCheck();
+                    Console.WriteLine("Please enter your password");
+                    string PassWd = Console.ReadLine();
+                    Paswd.UC_8_PasswordValidation(PassWd);
+                    Console.WriteLine(Paswd.UC_8_PasswordValidation(PassWd));
+                    break;
+                case 9:
+                    PatternCheck EmailRegex = new PatternCheck();
+                    List<string> sampleEmailList = new List<string>()
+                    {
+                        "abc@yahoo.com",
+                        "abc-100@yahoo.com",
+                        "abc.100@yahoo.com",
+                        "abc111@abc.com",
+                        "abc-100@abc.net",
+                        "abc.100@abc.com.au",
+                        "abc@1.com",
+                        "abc@gmail.com.com",
+                        "abc+100@gmail.com"
+                    };
+
+                    foreach (string item in sampleEmailList)
+                    {
+                        if (EmailRegex.EmailValidation(item))
+                            if (EmailRegex.allEmailValidation(item))
+                            {
+                                Console.WriteLine(EmailRegex.EmailValidation(item));
+                                Console.WriteLine(EmailRegex.allEmailValidation(item));
+                                Console.WriteLine("Valid Email");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid Email");
+                            }
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
         }
     }
 }
